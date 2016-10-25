20aa97945493c04f840db76840573ed9a95520f3
snoupyrelete <drob8896@740s-pl-129.local>
2016-10-25 09:20:33 -0600

    Setup for CardGame

---
+++ CardGame/Model/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  CardGame
+//
+//  Created by Robson, Dylan on 10/25/16.
+//  Copyright © 2016 Robson, Dylan. All rights reserved.
+//
+
+import Foundation
+
+class Card {
+//    var backImage: UIImage
+//    var isFaceUP : Bool
+    
+}
---
+++ CardGame/Model/PlayingCard.swift
@@ -0,0 +1,17 @@
+//
+//  PlayingCard.swift
+//  CardGame
+//
+//  Created by Robson, Dylan on 10/25/16.
+//  Copyright © 2016 Robson, Dylan. All rights reserved.
+//
+
+import Foundation
+
+class PlayingCard: Card {
+    override init() {
+//        var suit: String
+//        var color: UIColor
+        
+    }
+}
