ba5ecbd761ab16039f2f339dc1e1b3a48c9753e3
snoupyrelete <drob8896@740s-pl-129.local>
2016-10-27 09:51:34 -0600

    Started to test out deck model

---
+++ CardGame/Model/Deck.swift
@@ -0,0 +1,19 @@
+//
+//  Deck.swift
+//  CardGame
+//
+//  Created by Robson, Dylan on 10/27/16.
+//  Copyright © 2016 Robson, Dylan. All rights reserved.
+//
+
+import Foundation
+
+class Deck
+{
+    private var size: Int
+    
+    init(numberOfCards)
+    {
+        size = numberOfCards
+    }
+}
